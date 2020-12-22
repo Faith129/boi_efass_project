179cb1c5c9bcbfa40731f55fb34ddd3b50efb298
xamuel17 <unachukwu.samuel@gmail.com>
2020-12-22 13:56:53 +0100

    first commit

---
+++ src/main/java/com/efass/EfassBackApplication.java
@@ -0,0 +1,13 @@
+package com.efass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfassBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfassBackApplication.class, args);
+	}
+
+}
