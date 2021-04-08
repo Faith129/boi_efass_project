056d4cc2909c57fef1719ef6c10c286f6a54cb0f
xamuel17 <unachukwu.samuel@gmail.com>
2021-04-08 21:38:30 +0100

    sam-sheet300-new-changes

---
+++ src/main/java/com/efass/sheet/mmfbr300/sheet300TemporaryRepository.java
@@ -0,0 +1,15 @@
+package com.efass.sheet.mmfbr300;
+
+import java.util.ArrayList;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface sheet300TemporaryRepository  extends CrudRepository<sheet300TemporaryDAO, Integer>{
+
+	
+	@Query(value = "SELECT * FROM EFASS.MMFBR_300 order by code ascs", nativeQuery = true)
+	ArrayList<sheet300TemporaryDAO> findAllOrderByCode();
+	
+
+}
