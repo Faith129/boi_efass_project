a1adcfda789bee10b34ab0912ea2697b80d066af
engineerLiberty <libertyimobi@gmail.com>
2023-11-16 12:16:20 +0100

    Added excel upload implementation for sheet100

---
+++ src/main/java/com/efass/excelUpload/controller/ExcelUploadController.java
@@ -0,0 +1,25 @@
+package com.efass.excelUpload.controller;
+
+import com.efass.excelUpload.service.Sheet100Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
+
+@RestController("/api/v1")
+@RequiredArgsConstructor
+public class ExcelUploadController {
+
+    private final Sheet100Service sheet100Service;
+    @PostMapping("uploadSheet/{sheet100}")
+    public ResponseEntity<?> uploadSheet(@RequestParam("file") MultipartFile file, @PathVariable String sheet100) {
+        sheet100Service.saveSheetToDataBase(file,sheet100);
+        return ResponseEntity.ok(Map.of("Message","Sheet100 data uploaded and saved to database"));
+    }
+
+}
